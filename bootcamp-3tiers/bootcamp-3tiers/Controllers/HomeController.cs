21716dddb8cef3a72b6d5ac50aebf227a3f77f1a
mohmohoo <mohmohoo85@gmail.com>
2018-03-05 15:20:06 +0000

    Connect to api project

--- bootcamp-3tiers/bootcamp-3tiers/Controllers/HomeController.cs
+++ bootcamp-3tiers/bootcamp-3tiers/Controllers/HomeController.cs
@@ -1,8 +1,24 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace bootcamp_3tiers.Controllers
 {
+    public class Details
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Nationality { get; set; }
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -12,11 +28,17 @@ namespace bootcamp_3tiers.Controllers
             return View();
         }
 
-        public ActionResult List()
+        public async Task<ActionResult> List()
         {
-            DetailsRepository details = new DetailsRepository();
-            var userDetails = details.GetDetails();
+            var userDetails = await GetDetailsList();
             return View(userDetails);
+        }
+
+        private async Task<IEnumerable<Details>> GetDetailsList()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:50824/api/values");
+            return await response.Content.ReadAsAsync<IEnumerable<Details>>();
         }
     }
 }
