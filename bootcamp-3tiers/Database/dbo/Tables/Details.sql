86029c99a15d5ee305ccc01b321d40168c66807e
James Whiteley <j.s.whiteley@sky.com>
2018-03-06 11:49:58 +0000

    added database side of things

---
+++ bootcamp-3tiers/Database/dbo/Tables/Details.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Details] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (100) NULL,
+    [LastName]    VARCHAR (100) NULL,
+    [Age]         INT           NULL,
+    [Nationality] VARCHAR (100) NULL,
+    CONSTRAINT [PK_Details] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
